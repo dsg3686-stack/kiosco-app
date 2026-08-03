edba9043c9d87441d46086d856313eb53f76ca3c
dsg3686-stack <dsg3686@gmail.com>
2026-08-02 23:52:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,20 @@ pool.query(`
     precio DECIMAL(10,2)
   )
 `).catch(err => console.error("Error al crear tabla:", err));
-
+app.get('/productos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM productos');
+    let html = '<h2>Lista de Productos</h2><ul>';
+    result.rows.forEach(p => {
+      html += `<li>${p.nombre} - Costo: $${p.costo} - Precio: $${p.precio}</li>`;
+    });
+    html += '</ul><br><a href="/">Volver a cargar producto</a>';
+    res.send(html);
+  } catch (err) {
+    console.error(err);
+    res.send('Error al obtener productos');
+  }
+});
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
